d1888597e08db1dcb1362868fb032548d6736335
Guillermo Bautista Cid <gbautista0@gmail.com>
2021-02-04 16:08:35 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -37,18 +37,17 @@ class CampsiteInfo extends Component {
     }
 
     render() {
-        if(this.props.campsite) {
+        if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
-        } else {
-            return (
-                <div/>
-            );
         }
+        return <div />;
     }
 }
 
